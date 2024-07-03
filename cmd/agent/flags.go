5212b20396383683ec45f4a1579443119f2696b7
dmitrijmerkulov <drankar.hardhvn@gmail.com>
2024-07-03 14:33:03 +0300

    feat: add flags functional for server and agent

--- cmd/agent/flags.go
+++ cmd/agent/flags.go
@@ -1,15 +1,18 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"time"
+)
 
 var flagRunEndpoint string = "localhost:8080"
-var reportInterval int64 = 10
-var pollInterval int64 = 2
+var reportInterval time.Duration = 10
+var pollInterval time.Duration = 2
 
 func parseFlags() {
 	flag.StringVar(&flagRunEndpoint, "a", "localhost:8080", "address and port to run server")
-	flag.Int64Var(&reportInterval, "r", 10, "report Interval for metrics")
-	flag.Int64Var(&pollInterval, "p", 2, "pool Interval for metrics")
+	flag.DurationVar(&reportInterval, "r", time.Duration(10*time.Second), "report Interval for metrics")
+	flag.DurationVar(&pollInterval, "p", time.Duration(2*time.Second), "pool Interval for metrics")
 
 	flag.Parse()
 }
